901df5b6cff4c264827d5db51f65dfda12d92390
daryl <daryldhagz.21@gmail.com>
2025-12-09 15:12:29 +0000

    add remotepatterns and set default page

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,15 @@ const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
   images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'image.tmdb.org',
+        pathname: '/t/p/**',
+        port: '',
+        search: ''
+      }
+    ],
     domains: ['image.tmdb.org'],
   },
 };
